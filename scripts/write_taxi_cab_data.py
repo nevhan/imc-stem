30e956dbc8b3edb642dddb22f97d27f7c1932d67
Bradley McElroy <bradley.mcelroy@imc.com>
2018-07-18 01:33:01 +0000

    Add taxi cab example

--- scripts/write_taxi_cab_data.py
+++ scripts/write_taxi_cab_data.py
@@ -1,4 +1,5 @@
 import gzip
+import os
 from io import BytesIO
 from pathlib import Path
 from urllib.parse import urlparse
@@ -7,12 +8,13 @@ import requests
 import urllib3
 from lxml import html
 from minio import Minio
+from minio.error import NoSuchKey
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 chunksize = 200 * 1024 ** 2  # 200MB
 mc = Minio('minio.opt-pilot.svc.hkcc.ks:9000', 'opt-minio-dev', 'opt-minio-dev', secure=False)
-
+bucket_name = 'stem-pres'
 
 def get_data_links():
     resp = requests.get('http://www.nyc.gov/html/tlc/html/about/trip_record_data.shtml')
@@ -22,13 +24,26 @@ def get_data_links():
     return list(zip(links, names))
 
 
+def minio_exists(bucket, f):
+    try:
+        mc.stat_object(bucket, f)
+        return True
+    except NoSuchKey:
+        return False    
+
+
 def main():
     for link, name in get_data_links():
         resp = requests.get(link, stream=True, verify=False)
+        filename = '%s.gz' % name
+        if minio_exists(bucket_name, filename):
+            print('skipping %s' % filename)
+            continue
+        
         raw = BytesIO()
 
         print('{0} {1:0.2f} MB'.format(name, int(resp.headers['Content-Length']) / 1024 ** 2))
-
+        
         with gzip.GzipFile(fileobj=raw, mode='wb') as f:
             for content in resp.iter_content(chunk_size=chunksize):
                 print('  writing %s' % len(content))
@@ -36,7 +51,7 @@ def main():
 
         length = raw.tell()
         raw.seek(0)
-        mc.put_object('stem-pres', name, raw, length)
+        mc.put_object(bucket_name, filename, raw, length)
 
 
 if __name__ == '__main__':
